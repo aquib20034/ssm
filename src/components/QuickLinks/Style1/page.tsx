b47f7fbeb4cb204e18704d5efc2b1b8b3ba1e1b9
aquib20034 <aquib20034@gmail.com>
2023-05-25 22:01:22 +0500

    updated - point 3-6 covered
    Use HeadingS1 component with relevant variant and headingLevel props.
    In the components directory, create a directory with the name BlogCard. Create three more directories namely Style1, Style2 and Style3 for all three blog card components present on page.
    Create a directory namely QuickLinks. Inside this directory make another directory Style1 for QuickLinks card present on design.
    Component Export naming convention will be like: BlogCardS1, BlogCardS2, BlogCardS3, QuickLinksS1, etc.
    Use module SCSS with BEM naming convention for styling.

---
+++ src/components/QuickLinks/Style1/page.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import styles from "./index.module.scss";
+import Link from "next/link";
+
+type Props = {
+  routes: {
+    title: string;
+    path: string;
+  }[];
+};
+
+const QuickLinksS1: FunctionComponent<Props> = ({ routes }) => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default QuickLinksS1;
